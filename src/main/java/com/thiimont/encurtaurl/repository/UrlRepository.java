5345b6a4ffb73db9f88f074a02283dcef81b3a17
Thiago M. <185578934+thiimont@users.noreply.github.com>
2026-01-06 01:51:11 -0300

    feat(repository): add derived queries that finds URLs by user UUID to UrlRepository

--- src/main/java/com/thiimont/encurtaurl/repository/UrlRepository.java
+++ src/main/java/com/thiimont/encurtaurl/repository/UrlRepository.java
@@ -1,13 +1,17 @@
 package com.thiimont.encurtaurl.repository;
 
 import com.thiimont.encurtaurl.model.Url;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface UrlRepository extends JpaRepository<Url, Long> {
+    Optional<Url> findByUuidAndUserUuid(UUID uuidUrl, UUID uuidUser);
+    Page<Url> findAllByUserUuid(UUID uuidUser, Pageable pageable);
     Optional<Url> findByShortCode(String shortCode);
-    boolean existsByShortCode(String shortCode);
 }
