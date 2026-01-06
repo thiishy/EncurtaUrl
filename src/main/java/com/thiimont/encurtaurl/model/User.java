6933f399b7816a8d761c09c7d4b47b87bc732063
Thiago M. <185578934+thiimont@users.noreply.github.com>
2026-01-06 01:47:11 -0300

    feat(model): implement UserDetails methods in User

--- src/main/java/com/thiimont/encurtaurl/model/User.java
+++ src/main/java/com/thiimont/encurtaurl/model/User.java
@@ -51,4 +51,29 @@ public class User implements UserDetails {
         this.username = username;
         this.password = password;
     }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
