8d3f077178c0ed271a1b4f1482ea1c1856a43e29
Thiago M. <185578934+thiimont@users.noreply.github.com>
2026-01-06 01:51:56 -0300

    feat(controller): create AuthController for authentication endpoints

---
+++ src/main/java/com/thiimont/encurtaurl/controller/AuthController.java
@@ -0,0 +1,36 @@
+package com.thiimont.encurtaurl.controller;
+
+import com.thiimont.encurtaurl.dto.request.LoginRequestDTO;
+import com.thiimont.encurtaurl.dto.request.RegisterRequestDTO;
+import com.thiimont.encurtaurl.dto.response.LoginResponseDTO;
+import com.thiimont.encurtaurl.dto.response.RegisterResponseDTO;
+import com.thiimont.encurtaurl.repository.UserRepository;
+import com.thiimont.encurtaurl.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserRepository userRepository;
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@Valid @RequestBody LoginRequestDTO request) {
+        LoginResponseDTO response = authService.authenticateUser(request);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponseDTO> register(@Valid @RequestBody RegisterRequestDTO request) {
+        RegisterResponseDTO response = authService.registerUser(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
