ab9d1e91555b53ab324d2ca44665a00fd233f4ad
Thiago M. <185578934+thiimont@users.noreply.github.com>
2026-01-06 11:51:00 -0300

    feat(repository): create derived query to check if short code is active

--- src/main/java/com/thiimont/encurtaurl/repository/UrlRepository.java
+++ src/main/java/com/thiimont/encurtaurl/repository/UrlRepository.java
@@ -1,6 +1,7 @@
 package com.thiimont.encurtaurl.repository;
 
 import com.thiimont.encurtaurl.model.Url;
+import com.thiimont.encurtaurl.model.UrlStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,4 +15,5 @@ public interface UrlRepository extends JpaRepository<Url, Long> {
     Optional<Url> findByUuidAndUserUuid(UUID uuidUrl, UUID uuidUser);
     Page<Url> findAllByUserUuid(UUID uuidUser, Pageable pageable);
     Optional<Url> findByShortCode(String shortCode);
+    boolean existsByShortCodeAndStatus(String shortCode, UrlStatus status);
 }
