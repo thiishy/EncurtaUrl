282664456d643f0fb93954a27ee718e92d98f4fc
Thiago M. <185578934+thiimont@users.noreply.github.com>
2026-01-08 18:42:10 -0300

    feat(config): validate JWT from existing cookie in SecurityFilter

--- src/main/java/com/thiimont/encurtaurl/configuration/security/SecurityFilter.java
+++ src/main/java/com/thiimont/encurtaurl/configuration/security/SecurityFilter.java
@@ -2,14 +2,15 @@ package com.thiimont.encurtaurl.configuration.security;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.apache.logging.log4j.util.Strings;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
 
 import java.io.IOException;
 import java.util.Optional;
@@ -21,9 +22,10 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String authorizedHeader = request.getHeader("Authorization");
-        if (Strings.isNotEmpty(authorizedHeader) && authorizedHeader.startsWith("Bearer ")) {
-            String token = authorizedHeader.substring("Bearer ".length());
+        Cookie cookie = WebUtils.getCookie(request, "token");
+
+        if (cookie != null) {
+            String token = cookie.getValue();
             Optional<JWTUserData> optUser = tokenConfig.validateToken(token);
 
             if(optUser.isPresent()) {
@@ -31,6 +33,7 @@ public class SecurityFilter extends OncePerRequestFilter {
                 UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userData.uuid(), null, null);
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
+
             filterChain.doFilter(request, response);
         } else {
             filterChain.doFilter(request, response);
